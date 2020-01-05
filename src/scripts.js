e9c9522e38afaea68a9726a729b79678b56102db
Sirag Ahmed <ajj.siraj@gmail.com>
2020-01-05 22:34:05 +0200

    completed welcome part of homepage

--- src/scripts.js
+++ src/scripts.js
@@ -1,18 +1,8 @@
 $(document).ready(function () {
-
+    
+    //Mobile menu collapse button animation toggle
     $('#toggler-btn').on('click', function(){
         $('#clpse-icon').toggleClass("fa-arrow-up-animated");
-        // var state = $('#toggler-btn').attr('aria-expanded');
-        // if(state){
-        //     console.log("the if statement passed. Expanded is: ", state);
-        //     $('#clpse-icon').removeClass("fa-arrow-up-animated").addClass("fa-arrow-down-animated");
-        //     // $('#clpse-icon').addClass("fa-arrow-down-animated");
-        // }
-
-        // else{
-        //     $('#clpse-icon').removeClass("fa-arrow-down-animated").addClass("fa-arrow-up-animated");
-        //     // $('#clpse-icon').addClass("fa-arrow-up-animated");
-        // }
     });
 
     //This idea could work, I'll keep it for reference:
