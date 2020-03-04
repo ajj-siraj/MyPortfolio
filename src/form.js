83d59d193cb1951e7dbb1b4f37cfeeedeabb1e74
Sirag Ahmed <ajj.siraj@gmail.com>
2020-03-04 23:59:46 +0200

    added form validation using JS and stored form info in localStorage, to be viewed in form.html

---
+++ src/form.js
@@ -0,0 +1,75 @@
+const formSendBtn = document.querySelector('footer button');
+const formInfo = {
+    contactName: document.querySelector('#contact-name'),
+    contactEmail: document.querySelector('#contact-email'),
+    contactMsg: document.querySelector('#contact-message'),
+}
+const errorLocation = formSendBtn.parentNode.parentNode;
+
+const emailRegex = /[^@]+@[^@]+\.[a-zA-Z]{2,}/;
+
+
+formSendBtn.addEventListener('click', formRedirect);
+
+
+
+function formRedirect(e) {
+    // 
+    // for (let field in formInfo){
+    //     localStorage.setItem(field,)
+    // }
+
+    e.preventDefault();
+    verificationResult = formVerify();
+
+    if (verificationResult === "allGood") {
+        if (document.querySelector(".errormsg") !== null) {
+            formSendBtn.parentNode.parentNode.removeChild(document.querySelector(".errormsg"));
+        }
+
+        //redirection and localstorage logic
+        for(let field in formInfo){
+            // console.log(formInfo[field].value);
+            localStorage.setItem(field, formInfo[field].value);
+        }
+        window.location.href = "./form.html";
+        
+    } else {
+        //show error msg
+        if (document.querySelector(".errormsg") !== null) {
+            formSendBtn.parentNode.parentNode.removeChild(document.querySelector(".errormsg"));
+        }
+
+        if (verificationResult === "nameInvalid" || verificationResult === "msgInvalid") {
+            const errorDiv = document.createElement('div');
+            errorDiv.appendChild(document.createTextNode("You left some empty fields."));
+            errorDiv.classList.add("errormsg", "text-center", "alert", "alert-danger");
+            errorLocation.insertBefore(errorDiv, formSendBtn.parentNode);
+        }
+        if (verificationResult === "emailInvalid") {
+            const errorDiv = document.createElement('div');
+            errorDiv.appendChild(document.createTextNode("Please enter a valid email."));
+            errorDiv.classList.add("errormsg", "text-center", "alert", "alert-danger");
+            errorLocation.insertBefore(errorDiv, formSendBtn.parentNode);
+        }
+
+
+    }
+}
+
+//returns strings "nameInvalid", "emailInvaild", "msgInvalid" or "allGood"
+function formVerify() {
+    if (formInfo.contactMsg.value !== "") {
+        if (emailRegex.test(formInfo.contactEmail.value)) {
+            if (formInfo.contactName.value !== "") {
+                return "allGood";
+            }
+            return "nameInvalid";
+        }
+        return "emailInvalid";
+    }
+
+    return "msgInvalid";
+}
+
+export {formInfo};
--- src/scripts.js
+++ src/scripts.js
@@ -1,35 +1,15 @@
-let image1 = new Image();
-image1.src = "./img/thumbs/documentation-project.gif";
-let image2 = new Image();
-image2.src = "./img/thumbs/survey-project.gif";
-let image3 = new Image();
-image3.src = "./img/thumbs/guitarstore-project.gif";
-
 $(document).ready(function () {
+    
+    //Initialize gifs to pre-load even before hover.
+    let image1 = new Image();
+    image1.src = "./img/thumbs/documentation-project.gif";
+    let image2 = new Image();
+    image2.src = "./img/thumbs/survey-project.gif";
+    let image3 = new Image();
+    image3.src = "./img/thumbs/guitarstore-project.gif";
 
 
-    // $('.welcome-message').hide();
-    // $('.view-more-btn').hide();
-    // $('.about-message').hide();
-    // $('.projects-message').hide();
-    // $('.project-card').hide();
-    // // $('footer').hide();
-    // $('.welcome-message').fadeIn(1000);
-    // $('.view-more-btn').fadeIn(2000);
-    // $(window).scroll(function () {
-    //     if ($(this).scrollTop() > 250) {
-    //         $('.about-message').fadeIn(1000);
-
-    //     }
-    //     if ($(this).scrollTop() > 1500) {
-    //         $('.projects-message').fadeIn(1000);
-    //         $('.project-card').fadeIn(1000);
-    //     }
-
-
-    // });
-
-     $("#project-documentation").hover(
+    $("#project-documentation").hover(
         function () {
             $(this).attr("src", image1.src);
         },
